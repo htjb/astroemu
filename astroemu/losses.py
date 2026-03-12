2210c179494c97f15779b4641909d25798b4bf20
htjb <harrytjbevins@yahoo.co.uk>
2026-03-12 14:09:57 +0000

    adding kl loss function

--- astroemu/losses.py
+++ astroemu/losses.py
@@ -16,3 +16,23 @@ def mse(predictions: jnp.ndarray, targets: jnp.ndarray) -> jnp.ndarray:
         jnp.ndarray: Scalar MSE loss.
     """
     return jnp.mean((predictions - targets) ** 2)
+
+
+@jax.jit
+def kl(
+    predictions: jnp.ndarray, targets: jnp.ndarray, noise: jnp.ndarray
+) -> jnp.ndarray:
+    """Kullback-Leibler divergence loss.
+
+    From https://ui.adsabs.harvard.edu/abs/2025MNRAS.544..375B/abstract.
+
+    Args:
+        predictions (jnp.ndarray): Predicted probability distributions.
+        targets (jnp.ndarray): Target probability distributions.
+        noise (jnp.ndarray): Some estimate of noise in the data.
+
+    Returns:
+        jnp.ndarray: Scalar KL divergence loss.
+    """
+    rmse = jnp.sqrt(jnp.mean((predictions - targets) ** 2))
+    return predictions.shape[1] / 2 * (rmse / noise) ** 2
